630590c4980b4f5a3441fb328a6b37db8dfe8100
Vanessa0122 <yp4@ualberta.ca>
2020-01-14 13:23:48 -0700

    splitted url

--- httpclient.py
+++ httpclient.py
@@ -22,7 +22,7 @@ import sys
 import socket
 import re
 # you may use urllib to encode data appropriately
-import urllib.parse
+from urllib.parse import urlsplit
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -33,18 +33,20 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
-
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
         return None
 
     def get_code(self, data):
-        return None
+        #Version, code, message 
+        code = int(data.splitlines()[0].split(' '))
+        print("CODE: ", code)
+        return code 
 
     def get_headers(self,data):
-        return None
+        headers = data.split('\r\n\r\n')[0]
+        return headers 
 
     def get_body(self, data):
         return None
@@ -67,10 +69,29 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+    def request_builder(self, method, path, headers):
+        payload = '{} {} HTTP/1.1\r\n'.format(method, path)
+
+        for key, value in headers.items():
+            payload += '{}: {}\r\n'.format(key, value)
+
+        payload += '\r\n'
+
+        
+        return payload
+
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        port, host, path = self.url_split(url)
+        headers = {
+            'Host': '{}'.format(host),
+            'User-Agent': 'curl/7.54.0',
+            'Accept': '*/*'
+        }
+
+        request = self.request_builder('GET', path, headers)
+        self.connect(host, port)
+        self.sendall(request)
+        data = self.recvall(self.socket)
 
     def POST(self, url, args=None):
         code = 500
@@ -83,6 +104,24 @@ class HTTPClient(object):
         else:
             return self.GET( url, args )
     
+    def url_split(self, url):
+        # URL looks like this: <scheme>://<netloc>/<path>;<params>?<query>#<fragment>
+        #https://github.com/python/cpython/blob/3.8/Lib/http/client.py#L1115     
+        #SplitResult(scheme='http', netloc='www.google.com', path='', query='', fragment='')
+        protocol, host, path = urlsplit(url)[:3]  #Cut off query and fragment
+        if path == '':
+            path = '/'
+
+        #By default, HTTP uses port 80 and HTTPS uses port 443
+        if protocol == 'http':
+            port = 80
+        elif protocol == 'https':             #But do we need to take care of https?
+            port = 443
+        else:
+            raise ValueError('Invalid protocol. Instruction: Enter a website with http protocol.')
+        return port, host, path
+
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
